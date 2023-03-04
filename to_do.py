7f5ad45988e4c43e1f7d5724f102a60a219753c6
Benjamin Williams Asidis <2E7F1FCE98@gmail.com>
2023-03-04 11:26:06 +0100

    ideas

--- to_do.py
+++ to_do.py
@@ -3,18 +3,26 @@
 import sys
 import datetime
 
+
 # help function
 
-def help(): 
-    sa = """Usage: 
-$ ./to_do add 
-# help function
 def help():
-    sa = """Usage :-
+    system_architecture = """Usage :-
 $ ./todo add "todo item"  # Add a new todo
 $ ./todo ls               # Show remaining todos
 $ ./todo del NUMBER       # Delete a todo
 $ ./todo done NUMBER      # Complete a todo
 $ ./todo help             # Show usage
 $ ./todo report           # Statistics"""
-    sys.stdout.buffer.write(sa.encode('utf8'))  """
+    sys.stdout.buffer.write(system_architecture.encode('utf8'))
+
+
+x = 'looked'
+
+
+def basic_me(age, name):
+    # This prints out "John is 23 years old."
+    print("I am %d, and my name is %s" % (age, name))
+
+
+basic_me(26,"Williams Sidis")
